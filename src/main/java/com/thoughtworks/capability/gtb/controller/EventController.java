4fca4be0f951c2df0e78a90d08bd239c209dd902
jingx <xin.jing1@thoughtworks.com>
2020-06-13 00:38:40 +0800

    [Xin] JsonSerialize complete

--- src/main/java/com/thoughtworks/capability/gtb/controller/EventController.java
+++ src/main/java/com/thoughtworks/capability/gtb/controller/EventController.java
@@ -1,15 +1,14 @@
 package com.thoughtworks.capability.gtb.controller;
 
+import com.fasterxml.jackson.databind.*;
 import com.thoughtworks.capability.gtb.vo.EventType;
 import com.thoughtworks.capability.gtb.vo.EventVo;
 import com.thoughtworks.capability.gtb.vo.UserVo;
-import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 /**
  * @author itutry
@@ -18,6 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class EventController {
+  private final ObjectMapper objectMapper;
+
+  @Autowired
+  public EventController(ObjectMapper objectMapper){
+    this.objectMapper = objectMapper;
+  }
 
   @GetMapping("/events/{id}")
   public EventVo getEventById(@PathVariable("id") String id) {
--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,21 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +27,46 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class EventVo {
 
   private String id;
   private String name;
   private EventType type;
+  @JsonSerialize(using = LocalDateTimeSerializer.class)
+//  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm")
   private LocalDateTime time;
+
+  @JsonUnwrapped
   private UserVo user;
+
+  private static class LocalDateTimeSerializer extends StdSerializer<LocalDateTime>
+
+  {
+
+    protected LocalDateTimeSerializer() {
+      super(LocalDateTime.class);
+    }
+
+    @Override
+    public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+      gen.writeNumber(value.toInstant(ZoneOffset.of("+8")).toEpochMilli());
+    }
+  }
+
+  private static class LocalDateTimeDeserializer extends StdDeserializer<LocalDateTime>
+  {
+    protected LocalDateTimeDeserializer() {
+    super(LocalDateTime.class);
+  }
+
+    @Override
+    public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+
+      return null;
+    }
+
+
+  }
 }
